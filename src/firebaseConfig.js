8216b9ecdd4be9da7defdf30d651473735b18154
SaranSilv <sara.silva.114@ufrn.edu.br>
2025-07-06 12:44:56 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,16 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAl3ANQrBh8XgIpbJXfDA3P4peVE2IMkIs",
-  authDomain: "sitecta-18bf9.firebaseapp.com",
-  projectId: "sitecta-18bf9",
-  storageBucket: "sitecta-18bf9.firebasestorage.app",
-  messagingSenderId: "990473376468",
-  appId: "1:990473376468:web:00835c1605cc9d3b546141",
-  measurementId: "G-VTXKL4S19C",
+  //configs firebase
 };
 
 // Initialize Firebase
